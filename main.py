df639914f16bf3817e8ec315fa0e6e5499b056a2
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-13 12:30:36 +0100

    chore: refactor to OOP

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 from io import StringIO
 
+from report import Report
+
 
 def check_reports(file: StringIO):
     count = 0
     for report in file:
         levels=list(map(int, report.split()))
+        report = Report(levels)
 
-        if levels[0] > levels[1]:
-            increases = -1
-        elif levels[0] < levels [1]:
-            increases = 1
-        else:
-            continue
-
-        if is_report_safe(increases, levels):
+        if report.is_safe():
             count += 1
     return count
-
-
-def is_report_safe(increases, levels):
-    for index in range(len(levels) - 1):
-        if 0 < (levels[index + 1] - levels[index]) * increases < 4:
-            is_safe = True
-        else:
-            is_safe = False
-            break
-    return is_safe
-
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
---
+++ report.py
@@ -0,0 +1,27 @@
+class Report:
+    def __init__(self, report_data):
+        self.report_data = report_data
+
+    def is_safe(self):
+        for index in range(len(self.report_data) - 1):
+
+            next_level = self.report_data[index + 1]
+            current_level = self.report_data[index]
+            difference = next_level - current_level
+
+            if self._is_increasing() and difference < 0:
+                return False
+
+            is_in_bounds = 0 < abs(difference) < 4
+
+            if not is_in_bounds:
+                return False
+
+        return True
+
+    def _is_increasing(self):
+        if self.report_data[0] > self.report_data[1]:
+            return False
+
+        if self.report_data[0] < self.report_data[1]:
+            return True
