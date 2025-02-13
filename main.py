a6c6612856c02e0b3bc3aa8f0204526bbb1db610
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-13 09:11:49 +0100

    chore: replace awkward dependencies to be able to test the code

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-f = open("input.txt", "r")
-count = 0
-for i in f:
-	j=list(map(int, i.split()))
+from io import StringIO
 
-	if j[0] > j[1]:
-		m = -1
-	elif j[0] < j [1]:
-		m = 1
-	else:
-		continue
 
-	for x in range(len(j) -1):
-		if 0 < (j[x + 1] -j [x] ) * m < 4:
-			t = True
-		else:
-			t = False
-			break
+def check_reports(file: StringIO):
+    count = 0
+    for inform in file:
+        j=list(map(int, inform.split()))
 
-	if t:
-		count += 1
+        if j[0] > j[1]:
+            m = -1
+        elif j[0] < j [1]:
+            m = 1
+        else:
+            continue
 
-print(count)
+        for x in range(len(j) -1):
+            if 0 < (j[x + 1] -j [x] ) * m < 4:
+                t = True
+            else:
+                t = False
+                break
+
+        if t:
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        print(check_reports(f))
