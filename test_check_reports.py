a232f2ddeb85ccb4ed50ba61b879fbed863e14f9
Emmanuel Valverde Ramos <evalverde@voxelgroup.net>
2025-02-13 09:16:02 +0100

    test: add golden master with the same input that we had

---
+++ test_check_reports.py
@@ -0,0 +1,18 @@
+import unittest
+import io
+from main import check_reports
+
+
+class TestCheckReports(unittest.TestCase):
+    def test_check_report(self):
+        input_data = io.StringIO("""7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9""")
+
+        expected_output = 2
+        print(input_data)
+        result = check_reports(input_data)
+        self.assertEqual(result, expected_output)
